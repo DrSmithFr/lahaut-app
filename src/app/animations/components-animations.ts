43fc2eff468266a820e40256b40d0de88edbd038
John D <johndaligault@gmail.com>
2023-06-26 17:48:49 +0200

    animation: speeding up list animation

--- src/app/animations/components-animations.ts
+++ src/app/animations/components-animations.ts
@@ -4,14 +4,14 @@ export const flyInList = {
   parent: trigger('list', [
     transition(':enter', [
       query('@flyIn', [
-        stagger(100, animateChild()),
+        stagger(90, animateChild()),
       ]),
     ])
   ]),
   children: trigger('flyIn', [
     state('in', style({transform: 'translateX(0)'})),
     transition('void => in', [
-      animate(1000, keyframes([
+      animate(600, keyframes([
         style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
         style({opacity: 1, transform: 'translateX(15px)',  offset: 0.3}),
         style({opacity: 1, transform: 'translateX(0)',     offset: 1.0})
